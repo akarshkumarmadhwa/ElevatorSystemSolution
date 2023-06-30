bca95d5e872435f4c4e6f6da6edb3ad687225925
akarsh-kumar <akarsh.kumar@i3systems.in>
2023-07-01 02:27:11 +0530

    added changes to move elvator

--- elevator_system/elevator/apps.py
+++ elevator_system/elevator/apps.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class ElevatorConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'elevator'
+
+    def ready(self):
+        '''
+        Running the another thread containing infinite loop
+        '''
+        from .move_elevators import RunThread
+        RunThread().start()
